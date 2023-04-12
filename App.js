f054db1d2b4925fad5902732f9cb87e85de06c0e
Fady Moukawam <famoukawam@alinma.com>
2023-04-12 11:47:34 +0300

    removing unused code

--- App.js
+++ App.js
@@ -1,54 +1,17 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {ThemeProvider, Button, useThemeMode, createTheme} from '@rneui/themed';
-
-const Test = () => {
-	const {mode, setMode} = useThemeMode();
-
-	return (
-		<Button onPress={() => setMode(mode === 'light' ? 'dark' : 'light')}>
-			my button
-		</Button>
-	);
-};
-
-const theme = createTheme({
-	lightColors: {
-		//generic
-		white: '#ffffff',
-		black: '#000000',
-		transparent: 'transparent',
-		//theme
-		primary: '#e7e7e8',
-		secondary: '',
-		disabled: '',
-		error: '',
-		success: '',
-		warning: '',
-		background: '',
-		//text
-		textPrimary: '',
-		textSecondary: '',
-		textDisabled: '',
-	},
-	darkColors: {
-		primary: '#000',
-	},
-	mode: 'light',
-});
+import {ThemeProvider} from '@rneui/themed';
+import {theme} from 'theme/theme';
+import Home from 'screens/home/Home';
 
 const App = () => {
-	console.log('sdsd');
 	return (
 		<ThemeProvider theme={theme}>
-			<SafeAreaProvider style={{flex: 1, backgroundColor: 'white'}}>
-				<Test />
+			<SafeAreaProvider>
+				<Home />
 			</SafeAreaProvider>
 		</ThemeProvider>
 	);
 };
-
-const styles = StyleSheet.create({});
 
 export default App;
