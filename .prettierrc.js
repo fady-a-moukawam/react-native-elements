6a55b3316fbb29b132ee3e7e3818b180d382cac6
Fady Moukawam <famoukawam@alinma.com>
2023-04-13 11:42:41 +0300

    fix: adding new linting rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,12 @@
 module.exports = {
 	arrowParens: 'avoid',
 	bracketSameLine: true,
-	bracketSpacing: false,
+	bracketSpacing: true,
 	singleQuote: true,
 	trailingComma: 'all',
 	tabWidth: 2,
 	useTabs: true,
 	printWidth: 80,
 	semi: true,
-	trailingComma: 'es5',
-	singleQuote: true,
 	endOfLine: 'lf',
 };
